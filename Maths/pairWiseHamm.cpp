0e1d0dd3842221f0b7f6200c4710fc0d60321b88
Tanmay--Tiwari88 <tiwari.tanma@gmail.com>
2021-04-29 11:30:46 +0530

    Day 6

---
+++ Maths/pairWiseHamm.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define lc "\n"
+#define fast_io ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(0)
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define int long long
+#define c(a,n) for(int i = 0; i < n; i++) cin >> a[i];
+#define ffor(n) for(int i = 0; i < n; i++)
+ 
+typedef vector<int> vi;
+typedef vector<float> vf;
+typedef vector<vi> vii;
+typedef vector<string> vs;
+typedef vector<long long> vll;
+typedef map<string,int> msi;
+typedef map<int,int> mii;
+typedef unordered_map<string,int> umsi;
+ 
+
+ int hammingDistance(const vector<int> &A) {
+    long long  ans=0;
+    int m=1e9+7;
+    int n=A.size();
+    for(int i=0;i<32;i++)
+    {
+        long long count=0;
+        for(auto j:A)
+        {
+           if(j & (1<<i) ) 
+           count++;
+        }
+        ans+=(count*(n-count)*2);
+    }
+    return ans%m;
+}
+ 
+int32_t main()
+{
+    fast_io;
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        
+    }
+    return 0;
+}
